b24fe301a9f86feff0b6e2d9d94e52063fe706f6
Samuelikz <62406705+samuelikz@users.noreply.github.com>
2021-02-24 20:39:56 -0300

    Add files via upload

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,15 @@
-import express, { request, response } from 'express';
+import 'reflect-metadata';
+import express from 'express';
+import "./database";
 
 const app = express();
 
-/**
-* GET => Buscar
-* POST => Salvar
-* PUT => Alterar
-* DELETE => Deletar
-* PATCH => Alteração Especifica
-*/
-
-/*
-https://localhost:3333/users
-*/
 app.get("/", (request, response) => {
     return response.json({mensage: "Rota da Api /get"});
 });
 
 app.post("/", (request, response) => {
-    //recebeu parar salvar
 return response.json({mensagem: "Dados foram salvos /post"});
 });
 
-app.listen(3333, () => console.log("Servidor Rodando!"));
+app.listen(3121, () => console.log("Servidor Rodando!"));
