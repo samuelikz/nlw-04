a6969239a1c9469450888fc65def6c0198ae60f0
Samuelikz <62406705+samuelikz@users.noreply.github.com>
2021-02-27 13:37:33 -0300

    Add files via upload

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,15 @@
-import 'reflect-metadata';
-import express from 'express';
-import "./database";
+import 'reflect-metadata'
+import express from 'express'
+import './database'
 
-const app = express();
+const app=express();
 
 app.get("/", (request, response) => {
-    return response.json({mensage: "Rota da Api /get"});
-});
+    return response.json({message: "Hello World!"})
+})
 
-app.post("/", (request, response) => {
-return response.json({mensagem: "Dados foram salvos /post"});
-});
+app.post("/", (request, response) =>{
+    return response.json({message: "Dados recebidos!"})
+})
 
-app.listen(3121, () => console.log("Servidor Rodando!"));
+app.listen(3333, () => console.log("Server is running!"));
